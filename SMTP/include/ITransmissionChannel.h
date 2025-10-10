b4d31df8d6880b29d1eaa85f80a671186a86f9ee
r2char <eduardbernatskyi@proton.me>
2025-10-11 01:20:00 +0300

    Change ITransmissionChannel to use vector instead of raw pointers

--- SMTP/include/ITransmissionChannel.h
+++ SMTP/include/ITransmissionChannel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 
 namespace ISXSMTP
 {
@@ -10,11 +11,11 @@ class ITransmissionChannel
 public:
 	// Takes as the arguments pointer to the data and its count
 	// Returns number of written bytes
-	virtual size_t Write(std::uint8_t* data, size_t count) = 0;
+	virtual size_t Write(std::vector<uint8_t>& buffer) = 0;
 
 	// Takes as the arguments buffer and its size (not in bytes but as the count)
 	// Returns number of written bytes to the buffer
-	virtual size_t Read(std::uint8_t* buffer, size_t count) = 0;
+	virtual size_t Read(std::vector<uint8_t>& buffer) = 0;
 
 	// Returns true if there's anything to read
 	// and false otherwise
