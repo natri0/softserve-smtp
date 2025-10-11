2c2e96effb07fdef7fd4ab98f66ada91cb7c31aa
r2char <eduardbernatskyi@proton.me>
2025-10-11 14:10:19 +0300

    Make ITransmissionChannel::Write accept rvalue

--- SMTP/include/ITransmissionChannel.h
+++ SMTP/include/ITransmissionChannel.h
@@ -11,7 +11,7 @@ class ITransmissionChannel
 public:
 	// Takes as the arguments pointer to the data and its count
 	// Returns number of written bytes
-	virtual size_t Write(std::vector<uint8_t>& buffer) = 0;
+	virtual size_t Write(const std::vector<uint8_t>& buffer) = 0;
 
 	// Takes as the arguments buffer and its size (not in bytes but as the count)
 	// Returns number of written bytes to the buffer
