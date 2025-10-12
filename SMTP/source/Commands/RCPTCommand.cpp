f4a15206eb7f0f96d1e165052447f25c37fbce24
r2char <eduardbernatskyi@proton.me>
2025-10-12 16:46:46 +0300

    Implement RCPT command

--- SMTP/source/Commands/RCPTCommand.cpp
+++ SMTP/source/Commands/RCPTCommand.cpp
@@ -8,7 +8,29 @@ ISXSMTP::RCPTCommand::RCPTCommand()
 
 std::vector<ISXSMTP::SMTPReply> ISXSMTP::RCPTCommand::Invoke(SMTPCommandArguments arguments)
 {
-	return { SMTPReply::CommandNotImplemented() };
+	if (arguments.context->state != SMTPStates::POST_MAIL && arguments.context->state != SMTPStates::POST_RCPT)
+		return { SMTPReply::BadSequenceOfCommands() };
+
+	SMTPString forward_path;
+	try
+	{
+		forward_path = arguments.arguments.at("forward_path");
+	}
+	catch (...)
+	{
+		return { SMTPReply::SyntaxError() };
+	}
+
+	/*if (!arguments.mailbox->IsMailboxAvailable(forward_path))
+		return { SMTPReply::MailboxUnavailable550() };*/
+
+	// there can be multiple recipients
+	// so delimiter uses to distinguish them
+	arguments.context->forward_path.Append(forward_path + ";");
+	
+	arguments.context->state = SMTPStates::POST_RCPT;
+
+	return { SMTPReply::OK() };
 }
 
 ISXSMTP::SMTPString ISXSMTP::RCPTCommand::GetName()
