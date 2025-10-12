08a488432385940993582e4e56769a48549c74ac
r2char <eduardbernatskyi@proton.me>
2025-10-12 16:32:20 +0300

    Update MAIL command to check if mailbox is available

--- SMTP/source/Commands/MAILCommand.cpp
+++ SMTP/source/Commands/MAILCommand.cpp
@@ -26,6 +26,11 @@ std::vector<ISXSMTP::SMTPReply> ISXSMTP::MAILCommand::Invoke(SMTPCommandArgument
 	arguments.context->reverse_path.Clear();
 	arguments.context->mail_data.Clear();
 
+	if (!arguments.mailbox->IsMailboxAvailable(reverse_path))
+	{
+		return { SMTPReply::MailboxUnavailable550() };
+	}
+
 	arguments.context->reverse_path.Append(reverse_path);
 
 	arguments.context->state.Set(SMTPStates::POST_MAIL);
