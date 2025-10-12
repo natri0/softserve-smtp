6f34d40a73bcb8ec5e54c97993107e4c93147532
r2char <eduardbernatskyi@proton.me>
2025-10-12 15:37:54 +0300

    Update SMTPCommandArguments

--- SMTP/include/Commands/SMTPCommandArguments.h
+++ SMTP/include/Commands/SMTPCommandArguments.h
@@ -12,6 +12,9 @@ struct SMTPCommandArguments
 	// key - arg name
 	// value - arg value
 	std::unordered_map<SMTPString, SMTPString> arguments;
+
+	std::shared_ptr<SMTPContext> context;
+	std::shared_ptr<ISMTPMailbox> mailbox;
 };
 
 }
--- SMTP/include/Commands/SMTPCommandBase.h
+++ SMTP/include/Commands/SMTPCommandBase.h
@@ -13,7 +13,7 @@ namespace ISXSMTP
 class SMTPCommandBase 
 {
 public:
-	virtual SMTPReply Invoke(SMTPCommandArguments arguments, std::shared_ptr<SMTPContext> context) = 0;
+	virtual SMTPReply Invoke(SMTPCommandArguments arguments) = 0;
 	virtual SMTPString GetName() = 0;
 	virtual SMTPString GetSyntax() = 0;
 };
