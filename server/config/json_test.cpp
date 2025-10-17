7f172cb5d3e6f537b529102b0975e262bc3287ca
natri0 <dev@natri.fyi>
2025-10-17 13:08:11 +0300

    json: tests for number & string parsing
    actually caught a few bugs in strings related to \uXXXX parsing.

---
+++ server/config/json_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include "json.h"
+
+#include <format>
+#include "formatutil/jsonvalue.h"
+#include "formatutil/optional.h"
+
+#define SUITE JSONParser
+#define T(nm) TEST(SUITE, nm)
+
+#define EXPECT_NO_VALUE(kind, str) do { \
+    const char *s = str; \
+    auto res = json::visit_##kind(s);\
+    EXPECT_FALSE(res.has_value()) << "'" str "' parsed successfully while it shouldn't've: " << std::format("{}", *res); \
+} while (0)
+
+#define EXPECT_VALUE_OF_TYPE(kind, str) do { \
+    const char *s = str; \
+    auto res = json::visit_element(s); \
+    EXPECT_TRUE(res.has_value()) << "'" str "' didn't parse successfully!"; \
+    EXPECT_EQ(json::Value::kind, res->type()) << "expected type " #kind " for '" #str "'. parsed json: " << std::format("{}", *res); \
+} while (0)
+
+T(visit_number__ReturnsNoneOnInvalidNumber) {
+    EXPECT_NO_VALUE(number, "");
+    EXPECT_NO_VALUE(number, "just some random text");
+    EXPECT_NO_VALUE(number, "some text and a number after: 123");
+}
+
+T(visit_number__ReturnsValidValueOnValidNumber) {
+    EXPECT_VALUE_OF_TYPE(Number, "123");
+    EXPECT_VALUE_OF_TYPE(Number, "0.1");
+    EXPECT_VALUE_OF_TYPE(Number, "1e9");
+}
+
+T(visit_string__ReturnsNoneOnInvalidString) {
+    EXPECT_NO_VALUE(string, "not a quoted string");
+    EXPECT_NO_VALUE(string, "\"only one quote");
+    EXPECT_NO_VALUE(string, "only one quote\"");
+    EXPECT_NO_VALUE(string, "\"invalid \\Escape sequence\"");
+}
+
+T(visit_string__ReturnsValidValueOnValidString) {
+    EXPECT_VALUE_OF_TYPE(String, "\"properly quoted, \\t \\n \\\" \\\\ \\/ \\b \\f \\r \\u0001 \\u0100 \\u1234 \"");
+}
