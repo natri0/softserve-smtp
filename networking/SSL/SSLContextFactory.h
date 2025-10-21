c6106c45844b4c080d21bd4655378f864f0dbbe6
AlKirh <oleksandra.kirhetova@gmail.com>
2025-10-21 17:46:05 +0300

    added crypto and branch and changed cmake a bit so it can find openssl stuff

---
+++ networking/SSL/SSLContextFactory.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <boost/asio/ssl.hpp>
+#include <memory>
+#include <string>
+
+namespace smtp::ssl {
+  class SSLContextFactory {
+  public:
+    using SSLContext = boost::asio::ssl::context;
+
+    static std::shared_ptr<SSLContext> createServerContext();
+
+    static std::shared_ptr<SSLContext> createClientContext();
+
+  private:
+    static inline const std::string certFilePath{"networking/SSL/certs/server.crt"};
+    static inline const std::string keyFilePath{"networking/SSL/certs/server.key"};
+    static inline const std::string dhParamsFilePath{"networking/SSL/certs/dh.pem"};
+
+    static void configureContext(SSLContext &ctx);
+  };
+}
