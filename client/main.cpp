3b8b555fd9179d5f4c7f48b7fea61c0ea015bb32
AlKirh <oleksandra.kirhetova@gmail.com>
2025-10-22 23:32:43 +0300

    deleted thread for client

--- client/main.cpp
+++ client/main.cpp
@@ -12,27 +12,23 @@ int main()
 
     EmailMessage e_msg;
 
-    std::thread client_thread_00([&]()
+    client00.start();
+
+    // temp loop till there's no proper email manager snd UI loop to keep server alive
+    uint8_t escape;
+    do
     {
-        client00.start();
-
-        // temp loop till there's no proper email manager snd UI loop to keep server alive
-        uint8_t escape;
-        do
-        {
-            std::cout << "FROM: ";
-            std::cin >> e_msg.from;
-            std::cout << "\nTO: ";
-            std::cin >> e_msg.to;
-            std::cout << "\nSubject: ";
-            std::cin >> e_msg.subj;
-            std::cout << "\nContent: ";
-            std::cin >> e_msg.body;
-            client00.sendMail(e_msg);
-            std::cout << "Want to proceed? Yes: 1\tNo: 0" << std::endl;
-            std::cin >> escape;
-        } while (escape == 0);
-    });
-
-    client_thread_00.join();
+        std::cout << "FROM: ";
+        std::cin >> e_msg.from;
+        std::cout << "\nTO: ";
+        std::cin >> e_msg.to;
+        std::cout << "\nSubject: ";
+        std::cin >> e_msg.subj;
+        std::cout << "\nContent: ";
+        std::cin >> e_msg.body;
+        client00.sendMail(e_msg);
+        std::cout << "Want to proceed? Yes: 1\tNo: 0" << std::endl;
+        std::cin >> escape;
+    }
+    while (escape == 0);
 }
