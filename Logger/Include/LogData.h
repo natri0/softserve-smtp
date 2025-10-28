b5da906b9b35be9882707e7e16b802b80930cf5f
liuba <kryshalkaliub@gmail.com>
2025-10-28 16:52:15 +0200

    extending the LogData cstruct with additional methods

--- Logger/Include/LogData.h
+++ Logger/Include/LogData.h
@@ -1,15 +1,36 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <thread>
 #include "LogLevel.h"
 
 
-struct LogData
-{
-	std::string msg;
-	std::string type;
-	std::string location;
-	LogLevel level;
-	std::thread::id thr_id;
+struct LogData {
+private:
+    std::string msg;
+    std::string type;
+    std::string location;
+    LogLevel level;
+    std::thread::id thr_id;
+    //void* object_ptr;
+public:
+    LogData(const std::string& m, const std::string& t, const std::string& loc,
+        const LogLevel& l, std::thread::id id) //void* ptr = nullptr)
+        : msg(m), type(t), location(loc), level(l), thr_id(id) { //, object_ptr(ptr) {
+    }
+
+    template<typename T>
+    LogData& operator<<(const T& value) {
+        std::ostringstream oss;
+        oss << value;
+        msg += oss.str();
+        return *this;
+    }
+
+    LogData& ref()
+    {
+        return *this;
+    }
+
 
 };
