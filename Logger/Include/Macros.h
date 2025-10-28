5990a24625090567f54ce0405914937e44da25b7
liuba <kryshalkaliub@gmail.com>
2025-10-28 16:45:26 +0200

    add some new macros

--- Logger/Include/Macros.h
+++ Logger/Include/Macros.h
@@ -10,12 +10,36 @@
 #define DEFAULT_PATH ""
 //#define DEFAULT_CONFIG false
 //#define DEFAULT_FLUSH true
+#define DEFAULT_LOG_LEVEL NO_LOG_LEVEL
 
 #define NO_LOG_LEVEL LogLevel::NONE
 #define PROD_LOG_LEVEL LogLevel::PROD
 #define DEBUG_LOG_LEVEL LogLevel::DEBUG
 #define TRACE_LOG_LEVEL LogLevel::TRACE
 
-#define DEFAULT_LOG_LEVEL NO_LOG_LEVEL
+#define INFO_TYPE "[INFO]"
+#define ERROR_TYPE "[ERROR]"
+#define WARNING_TYPE "[WARNING]"
 
 #define FUNCTION_NAME __FUNCTION__
+
+//#define LOG_GET_THIS()      __if_exists(this) { this } __if_not_exists(this) { 0 }
+
+#ifdef _MSC_VER
+#   define LOG_GET_FUNC()      __FUNCTION__
+#else
+#   define LOG_GET_FUNC()      __PRETTY_FUNCTION__
+#endif
+
+
+#define LOG_GET_FILE()      __FILE__
+
+
+#define IF_LOG_(level)   if (Logger::getInstance().blockLog(level)) { ; } else
+
+#define LOG(level,type) IF_LOG_(level) (Logger::getInstance()) += \
+    LogData("", type, LOG_GET_FUNC(), level, std::this_thread::get_id()).ref()
+
+#define LOG_INFO(level)           LOG(level, INFO_TYPE)
+#define LOG_WARNING(level)        LOG(level, WARNING_TYPE)
+#define LOG_ERROR(level)          LOG(level, ERROR_TYPE)
