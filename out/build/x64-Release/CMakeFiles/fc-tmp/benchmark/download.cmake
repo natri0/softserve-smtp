6e6a3efcc02f8fc6d1465f56ed9aba54806ad9d6
KryzhalkaLiuba <126720876+KryzhalkaLiuba@users.noreply.github.com>
2025-11-05 18:44:08 +0200

    Delete out/build directory

--- out/build/x64-Release/CMakeFiles/fc-tmp/benchmark/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for benchmark")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/user/source/repos/softserve-smtp/out/build/x64-Release/CMakeFiles/fc-tmp/benchmark/benchmark-gitclone.cmake")
-
-endblock()
