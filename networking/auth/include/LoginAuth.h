43dbafec681f9c8c5d96b13448db661ee4707d9f
Mykhailo <mmmalets10@gmail.com>
2025-11-14 02:19:29 +0200

    remove auth module

--- networking/auth/include/LoginAuth.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOGIN_AUTH
-#define LOGIN_AUTH
-#include "IAuthMethod.h"
-
-class LoginAuth final : public IAuthMethod {
-  bool authenticate(const std::string &user, const std::string &secret) override;
-};
-
-#endif
--- networking/auth/include/PlainAuth.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAIN_AUTH
-#define PLAIN_AUTH
-#include "IAuthMethod.h"
-
-class PlainAuth final : public IAuthMethod {
-  bool authenticate(const std::string &user, const std::string &secret) override;
-};
-
-#endif
