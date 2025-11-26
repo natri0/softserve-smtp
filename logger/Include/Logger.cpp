ea272ab611bc8704f22e985b14ddefc980a0b060
liuba <kryshalkaliub@gmail.com>
2025-11-26 10:28:09 +0200

    remove unnecessary locks

--- logger/Include/Logger.cpp
+++ logger/Include/Logger.cpp
@@ -169,7 +169,6 @@ void Logger::writeLogToFile(const LogData& data) {
 
     std::string file_output = std::vformat(data.format, std::make_format_args(data));
 
-    std::unique_lock lock(mutex);
     file << file_output << std::endl;
     file.flush();
 }
@@ -178,7 +177,6 @@ void Logger::writeLogToConsole(const LogData& data) {
     const ConsoleLog content = ConsoleLog{ data };
     std::string console_output = std::vformat(data.format, std::make_format_args(content));
 
-    std::unique_lock lock(mutex);
     std::cout << console_output << std::endl;
 }
 
